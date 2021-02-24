0e667093127922bcb9cdc2f6c4981747b630ff3b
blackjack2015 <qiangwang@comp.hkbu.edu.hk>
2021-02-24 13:11:58 +0800

    finish testing the functions of searching kernel and depth.

--- train.sh
+++ train.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # slurm batch script
-#SBATCH -o /home/comp/qiangwang/blackjack/once-for-all/kernel.out
-#SBATCH --nodes=1
+#SBATCH -o /home/comp/qiangwang/blackjack/once-for-all/width.out
+#SBATCH --nodes=2
 #SBATCH --ntasks-per-node=4
-#SBATCH -w hkbugpusrv05
+#SBATCH -w hkbugpusrv04,hkbugpusrv05
 
 # Train on Scene Flow training set
 PY="/usr/local/bin/python"
@@ -17,8 +17,8 @@ params="--mca pml ob1 --mca btl openib,vader,self --mca btl_openib_allow_ib 1  \
 	-x NCCL_SOCKET_IFNAME=ib0  \
 	-x NCCL_DEBUG=INFO  \
 	-x HOROVOD_CACHE_CAPACITY=0"
-hosts="-np 4 -H hkbugpusrv05:4"
-#hosts="-np 8 -H hkbugpusrv04:4,hkbugpusrv05:4"
+#hosts="-np 4 -H hkbugpusrv05:4"
+hosts="-np 8 -H hkbugpusrv04:4,hkbugpusrv05:4"
 
 # train the large super net
 #$MPIPATH/bin/mpirun --oversubscribe --prefix $MPIPATH $hosts -bind-to none -map-by slot \
@@ -28,4 +28,4 @@ hosts="-np 4 -H hkbugpusrv05:4"
 # shrink the kernel,depth,width
 $MPIPATH/bin/mpirun --oversubscribe --prefix $MPIPATH $hosts -bind-to none -map-by slot \
 	$params \
-	$PY train_ofa_stereo.py --task kernel
+	$PY train_ofa_stereo.py --task expand
